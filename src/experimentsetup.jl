9df5977d1e7e6cefb09a9568ff585eb59c76c99b
= <pjabardo@ipt.br>
2022-10-04 12:45:57 -0300

    Added docstrings to src/experimentsetup.jl
    The docstrings are mostly done. Should actually write the manual.

--- src/experimentsetup.jl
+++ src/experimentsetup.jl
@@ -4,7 +4,60 @@
 export ExperimentSetup, lastpoint, incpoint!, setpoint!, movenext!
 export finishedpoints, inputdevice, outputdevice
 
+"""
+`ExperimentSetup(idev, pts, odev)`
 
+Creates a structure that handles experiment setup.
+Experiment setup in this context is the set of input devices,
+output devices and points where the output devices should be
+positioned to measure the input devices.
+
+The output device (`odev`) is used to set the configuration corresponding to each
+point (`points`) where the input device (`idev`) will measure the data.
+
+This is done sequentially from the first point until the test ends.
+
+The point that was done is specified by the field `lastpoint`. Usually when an
+experiment begins, there was no previous point and `lastpoint=0`. Once the
+experiemnt begins, field `started` is set to `true`. The output device (`odev`)
+'moves' the system to the first point using method [`movenext!`](@ref).
+While there are points to be measured, the method [`movenext!`](@ref) returns `true`.
+Once the last point has been measured, [`movenext!`](@ref) returns `false` and
+the experiment is considered over.
+
+To 'move' the system to a new point (defined in field `points`), [`movenext!`](@ref)
+acts on the output device `odev`. Each parameter in `points` corresponds to an
+axis in `odev`. If `points` and `odev` were created with different names, a map can
+be specified.
+
+`ExperimentSetup` does not do any coordinate change. This should be defined outside,
+preferably by creating a new output device that handles the transformation.
+
+If the output device is a [`OutputDevSet`](@ref) and the points
+[`DaqPointsProduct`](@ref), the method [`movenext`](@ref) will move each
+output device independently and will check if the points associated with
+the axes of each individual output device that makes up the `OutputDevSet`
+actually changed.
+
+## Examples
+
+```julia-repl
+ulia> idev = TestDaq("Device1"); daqaddinput(idev, ["E1", "E2"]);
+
+julia> odev = TestOutputDev("fan", ["RPM"]);
+
+julia> pts = DaqPoints(vel=200:50:300);
+
+julia> setup = ExperimentSetup(idev, pts, odev, Dict("RPM"=>"vel"));
+
+julia> while movenext!(setup)
+           println(daqpoint(setup.points, lastpoint(setup)))
+       end
+[200.0]
+[250.0]
+[300.0]
+```
+"""
 mutable struct ExperimentSetup{IDev<:AbstractInputDev,Pts<:AbstractDaqPoints,ODev<:AbstractOutputDev} <: AbstractExperimentSetup
     "Index of last point measured"
     lastpoint::Int
@@ -164,7 +217,7 @@ function movenext!(pts::ExperimentSetup)
     moveto!(pts.odev, pax)
     incpoint!(pts)
 
-    if (lp+1) == numpoints(pts) # Last point
+    if (lp+1) > numpoints(pts) # Last point
         pts.started = false
         return false
     else
@@ -187,10 +240,24 @@ numpoints(pts::ExperimentSetup) = numpoints(pts.points)
 axesnames(pts::ExperimentSetup) = axesnames(pts.odev)
 numaxes(pts::ExperimentSetup) = numaxes(pts.odev)
 
+"Return the input device of the `ExperimentSetup`"
 inputdevice(pts::ExperimentSetup) = pts.idev
+"Return the output device of the `ExperimentSetup`"
 outputdevice(pts::ExperimentSetup) = pts.odev
 
 
+"""
+`movenext!(setup)`
+
+Move to the next point in the experiment when dealing with [`OutputDevSet`](@ref) meta
+device sets. If the experiment point is not the first one, it will act on the output
+devices if the next point changed it.
+
+It would seem intuitive to just call [`devposition`](@ref) and check if we are moving
+but some devices provide approximate values that might not correspond to the
+exact position specified by points. 
+
+"""
 function movenext!(pts::ExperimentSetup{IDev,Pts,ODev}) where {IDev<:AbstractInputDev,
                                                                Pts<:AbstractDaqPoints,
                                                                ODev<:OutputDevSet}
